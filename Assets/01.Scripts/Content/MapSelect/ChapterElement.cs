c072f45873809fd75ec6276452bd6058af02ccad
janghawon <98999600+janghawon@users.noreply.github.com>
2024-06-17 21:36:33 +0900

    0617

--- Assets/01.Scripts/Content/MapSelect/ChapterElement.cs
+++ Assets/01.Scripts/Content/MapSelect/ChapterElement.cs
@@ -9,6 +9,8 @@ using DG.Tweening;
 
 public class ChapterElement : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    public bool CanTryThisChapter { get; set; }
+
     [SerializeField] private MapDataSO _chapterData;
     [SerializeField] private UnityEvent<MapDataSO> _loadMapActiveEvent;
 
@@ -31,17 +33,30 @@ public class ChapterElement : MonoBehaviour, IPointerClickHandler, IPointerEnter
 
     public void OnPointerClick(PointerEventData eventData)
     {
+        if (!CanTryThisChapter)
+        {
+            ErrorText text = PoolManager.Instance.Pop(PoolingType.ErrorText) as ErrorText;
+            text.transform.SetParent(UIManager.Instance.CanvasTrm);
+            text.Erroring("준비 중 입니다!");
+
+            return;
+        }
+
         SelectThisChapter();
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
+        if (!CanTryThisChapter) return;
+
         transform.DOKill();
         transform.DOScale(Vector3.one * 1.05f, 0.2f).SetEase(Ease.OutBack);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
+        if (!CanTryThisChapter) return;
+
         transform.DOKill();
         transform.DOScale(Vector3.one, 0.2f);
     }
